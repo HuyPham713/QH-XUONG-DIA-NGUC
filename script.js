f953c3036d85f31553935de4768a83321dc73378
HuyPham713 <140079057+HuyPham713@users.noreply.github.com>
2024-08-10 21:35:47 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var a = document.querySelector("button.no");
-a.addEventListener("mousemove",change);
-function change()
-{
-    var i = Math.floor(Math.random()*500)+1;
-    var j = Math.floor(Math.random()*500)+1;
-    a.style.left = i+"px";
-    a.style.top = j+"px";
-}
